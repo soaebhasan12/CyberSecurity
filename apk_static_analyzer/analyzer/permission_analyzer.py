654310251f8ccc3362a6f593fbadbcf5fb46d226
unknown <saibuhasan22@gmail.com>
2026-02-12 22:30:08 +0530

    Detects dangerous permissions used by the app

---
+++ apk_static_analyzer/analyzer/permission_analyzer.py
@@ -0,0 +1,18 @@
+# Responsibility: Detects dangerous permissions used by the app
+
+DANGEROUS_PERMISSIONS = [ "android.permission.SEND_SMS", 
+                         "android.permission.READ_SMS", 
+                         "android.permission.RECORD_AUDIO", 
+                         "android.permission.ACCESS_FINE_LOCATION" 
+                         ]
+
+def analyze_permissions(apk_obj):
+  found = []
+  
+  permissions = apk_obj.get_permissions()
+  
+  for perm in permissions:
+    if perm in DANGEROUS_PERMISSIONS:
+      found.append(perm)
+  
+  return found
